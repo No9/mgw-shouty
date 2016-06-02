275f45f2a21e7df2a886fe4a24ab4ea184baf13f
AWhalley <awhalley@ie.ibm.com>
2016-06-02 14:43:25 +0100

    Added message.body validation

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 'use strict';
 
-// This code has a blatant disregard for errors that may happen...
-
 /**
  * This exported function is the factory for the policy runtime implementation.
  * It is called once during the policy setup phase.
  * @param config - Static configuration information that applies to this policy
  */
 module.exports = function (config) {
-  console.log('Creating: mgw-shouty');
 
-  // normally, I would do global setup, init and config parsing here,
-  // but this policy is too simple for all of that stuff.
+  // Do global init and config parsing here,
 
   /**
    * This is the policy runtime implementation, it gets called each time the
@@ -25,15 +21,31 @@ module.exports = function (config) {
     return function (props, context, flow ) {
      
      var resp = JSON.parse(context.message.body.toString());
+     if(!Array.isArray(resp)) {
+        var typeErr = {
+            name : 'FlowTypeError',
+            message: 'The type in message.body is not an Array'
+        }
+        flow.fail(typeErr);
+        return true;
+     }
 
      for(var i = 0; i < resp.length; i++) {
+
         if(typeof resp[i] === 'string') {
            resp[i] = resp[i].toUpperCase();
+        } else {
+        var elementErr = {
+            name : 'FlowTypeError',
+            message: 'The type of elements in message.body Array are not strings'
         }
-
+      
+           flow.fail(elementErr);
+           return true;
+        }
+      
       }
       context.message.body = resp;
-      console.log('Invoking mgw-shouty');
       flow.proceed();
-    }
+    };
 }
--- tests/test.js
+++ tests/test.js
@@ -3,10 +3,10 @@ var Policy = require('../');
 var p = Policy();
 
 test('policy is called', function(t) {
-   t.plan(1);
+   t.plan(2);
    var flow = {};
    flow.proceed = function() {
-      t.ok('Proceed Called');
+      t.ok(true, 'Proceed Called');
    };
    var b = new Buffer('["this is a test"]') ;  
    var context = { message : {
@@ -15,4 +15,40 @@ test('policy is called', function(t) {
    };
 
    p({}, context, flow);
+   t.equal(context.message.body[0], 'THIS IS A TEST', 'Uppercase test');
+});
+
+test('non array in the body throws', function(t) {
+  t.plan(2);
+  var flow = {};
+
+  flow.fail = function(err) {
+     t.equal(err.name, 'FlowTypeError', 'Error type');
+     t.equal(err.message, 'The type in message.body is not an Array');
+  };
+  var context = { message : {
+         body : 123
+      }
+  };
+
+  p({}, context, flow);  
+
+});
+
+test('non string in the array throws', function(t) {
+  t.plan(2);
+  var flow = {};
+
+  flow.fail = function(err) {
+     t.equal(err.name, 'FlowTypeError', 'Error type');
+     t.equal(err.message, 'The type of elements in message.body Array are not strings');
+  };
+  var b = new Buffer("[123]");
+  var context = { message : {
+         body : b
+      }
+  };
+
+  p({}, context, flow);  
+
 });
